c124cbd6b26de0d6a25007487c8cfacb4e5c2938
Mind1995Star <105137625+Mind1995Star@users.noreply.github.com>
2022-08-11 04:37:20 +0900

    update:mongoURI

--- controllers/metamask.controller.js
+++ controllers/metamask.controller.js
@@ -3,18 +3,15 @@ const MetamaskModel = require("../models/MetamaskAddress.model");
 exports.addAddress = async (req, res) => {
   try {
     const { walletAddress } = req.body;
-    // console.log("metamask address:", req.body);
     MetamaskModel.findOne({ address: walletAddress }).then((data) => {
       if (data) {
         console.log("Address already exists");
-        // errors.address = "Address already exists";
         return res.status(200).send("Address already exists");
       } else {
         const newAddress = new MetamaskModel({
           address: walletAddress,
         }).save();
         console.log("Success Address add");
-        // const data = await new MetamaskModel({ address: walletAddress }).save();
         return res.status(200).send("Registered in the database");
       }
     });
